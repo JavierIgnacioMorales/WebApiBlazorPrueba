0b3de5bb58cbd358349204d68f5b9bf4b991b8a2
Javier Morales <javier.morales@educacion.gob.ar>
2024-08-01 12:54:43 -0300

    SE INCLUYEN DATOS DE INIT GIT

--- obj/Debug/net6.0/BlazorCrudPrueba.AssemblyInfo.cs
+++ obj/Debug/net6.0/BlazorCrudPrueba.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorCrudPrueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5079fdf0c5b446e44fdaf3416bc5e3a73d2b9661")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorCrudPrueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorCrudPrueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
